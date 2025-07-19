35db730dec6aa1af710d6d95e3c010b893d4fa43
jake <you@example.com>
2025-07-20 01:57:55 +0700

    feat: Implemented Schemas for the management site.

---
+++ schemaTypes/index.ts
@@ -0,0 +1,13 @@
+import account from "./account";
+import booking from "./booking";
+import hotelRoom from "./hotelRoom";
+import user from "./user";
+import verificationToken from "./verificationToken";
+
+export const schemaTypes = [
+    user, 
+    account, 
+    booking, 
+    hotelRoom, 
+    verificationToken
+];
