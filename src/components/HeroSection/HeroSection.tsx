9cfcbd020486340d1a255e437ce4bc7236e07c5d
jake <you@example.com>
2025-07-30 01:15:11 +0700

    feat: implemented countUpNumber animation and image display on hero section.

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,3 +1,6 @@
+'use client'
+import Image from 'next/image';
+import CountUpNumber from '../CountUpNumber/CountUpNumber';
 
 const HeroSection = () => {
   return (
@@ -11,11 +14,68 @@ const HeroSection = () => {
                 Experience an Exquisite Hotel Immersed in Rich History and Timeless Elegance.
             </p>
             <button className="btn-primary">Get Started</button>
+
             {/* Room Description */}
+            <div className="flex justify-between mt-12">
+                <div className="flex gap-3 flex-col items-center justify-center">
+                    <p className="text-xs lg:text-xl text-center">Basic Room</p>
+                    {/* <CountUpNumber duration={3000} endValue={200}/> */}
+                </div>
+                <div className="flex gap-3 flex-col items-center justify-center">
+                    <p className="text-xs lg:text-xl text-center">Luxury Room</p>
+                    {/* <CountUpNumber duration={3000} endValue={200}/> */}
+                </div>
+                <div className="flex gap-3 flex-col items-center justify-center">
+                    <p className="text-xs lg:text-xl text-center">Suite Room</p>
+                    {/* <CountUpNumber duration={3000} endValue={200}/> */}
+
+                </div>
+                <div className="flex gap-3 flex-col items-center justify-center">
+                    <p className="text-xs lg:text-xl text-center">Presidential Room</p>
+                    {/* <CountUpNumber duration={3000} endValue={200}/> */}
+
+                </div>
+                
+            </div>
         </div>
+
         {/* Images */}
+        <div className="md:grid hidden gap-8 grid-cols-1">
+            <div className="rounded-2xl overflow-hidden h-48">
+                <Image 
+                    src='/images/hero-1.jpeg' 
+                    alt='hero-1' 
+                    width={300} 
+                    height={300} 
+                    className='img scale-animation'
+                />
+            </div>
+            <div className="grid grid-cols-2 gap-8 h-48">
+                <div className="rounded-2xl overflow-hidden">
+                    <Image 
+                        src='/images/hero-2.jpeg' 
+                        alt='hero-2' 
+                        width={300} 
+                        height={300} 
+                        className='img scale-animation'
+                    />
+                </div>
+                <div className="rounded-2xl overflow-hidden">
+                    <Image 
+                        src='/images/hero-3.jpeg' 
+                        alt='hero-3' 
+                        width={300} 
+                        height={300} 
+                        className='img scale-animation'
+                    />
+                </div>
+            </div>
+        </div>
+        
+        
+        
     </section>
-  )
-}
+  );
+};
 
 export default HeroSection
