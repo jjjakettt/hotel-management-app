4f1b607ca05598fad70f114b0b12ad9ad6e4a578
jake <you@example.com>
2025-08-11 23:52:44 +0700

    feat: fixed styling issue with booking button (color), removed animation from hero page.

--- src/components/HeroSection/ClientComponent.tsx
+++ src/components/HeroSection/ClientComponent.tsx
@@ -17,27 +17,25 @@ const ClientComponent: FC<Props> = (props) => {
         <section className="flex px-4 items-center gap-12 container mx-auto">
             <div className="py-10 h-full">
                 {heading1}
-
-                {/* Room Description */}
+                
                 <div className="flex justify-between mt-12">
-                    <div className="flex gap-3 flex-col items-center justify-center">
-                        <p className="text-xs lg:text-xl text-center">Basic Room</p>
-                        <CountUpNumber duration={3000} endValue={50}/>
+                    {/* <div className="flex gap-3 flex-col items-center justify-center">
+                        <p className="text-xs lg:text-xl text-center">Deluxe Room</p>
+                        <CountUpNumber duration={1000} endValue={5}/>
                     </div>
                     <div className="flex gap-3 flex-col items-center justify-center">
-                        <p className="text-xs lg:text-xl text-center">Luxury Room</p>
-                        <CountUpNumber duration={3000} endValue={50}/>
+                        <p className="text-xs lg:text-xl text-center">Premium Room</p>
+                        <CountUpNumber duration={1000} endValue={5}/>
                     </div>
                     <div className="flex gap-3 flex-col items-center justify-center">
-                        <p className="text-xs lg:text-xl text-center">Suite Room</p>
-                        <CountUpNumber duration={3000} endValue={50}/>
+                        <p className="text-xs lg:text-xl text-center">Grand Suite</p>
+                        <CountUpNumber duration={1000} endValue={5}/>
 
                     </div>
                     <div className="flex gap-3 flex-col items-center justify-center">
                         <p className="text-xs lg:text-xl text-center">Presidential Room</p>
-                        <CountUpNumber duration={3000} endValue={50}/>
-
-                    </div>
+                        <CountUpNumber duration={1000} endValue={5}/>
+                    </div> */}
                     
                 </div>
             </div>
