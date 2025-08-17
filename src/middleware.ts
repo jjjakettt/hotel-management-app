017e376466da65d7c42baa44e271652a47f222bc
jake <you@example.com>
2025-08-17 22:50:23 +0700

    feat: completed rating page. NEXT: implement rating API

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,8 @@
 export { default } from "next-auth/middleware";
 
-// export const config = {
-//     matcher: ["/user/:path*", "/api/:path*"],
-// };
 export const config = {
-    matcher: ["/user/:path*"],
+    matcher: ["/user/:path*", "/api/:path*"],
 };
+// export const config = {
+//     matcher: ["/user/:path*"],
+// };
