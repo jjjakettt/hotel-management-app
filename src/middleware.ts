2105a3422830373de181ba33ba918a4962f22dea
jake <you@example.com>
2025-08-21 01:26:03 +0700

    fix: removed api block in middleware so non-logged in users can see reviews.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-    matcher: ["/user/:path*", "/api/:path*"],
+    matcher: ["/user/:path*"],
 };
-// export const config = {
-//     matcher: ["/user/:path*"],
-// };
