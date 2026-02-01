7d4e40974dd24805d0f0cdc711f88321d4bc392c
jake <you@example.com>
2026-02-01 14:04:09 -0500

    feat: implement booking calendar tool.

--- sanity.config.ts
+++ sanity.config.ts
@@ -2,6 +2,7 @@ import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
 import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
+import {bookingCalendarPlugin} from './src/sanity/plugins/bookingCalendar'
 
 export default defineConfig({
   name: 'default',
@@ -12,7 +13,7 @@ export default defineConfig({
 
   basePath: "/studio",
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool(), visionTool(), bookingCalendarPlugin()],
 
   schema: {
     types: schemaTypes,
